8006aab74d7697ca77d729415ee3b117a2f7592c
KirbyBurkeJr <108311405+KirbyBurkeJr@users.noreply.github.com>
2025-02-16 17:10:02 -0800

    Update Hazard_Detection.v
    Updated labels for rs and rt address inputs.

--- src/design/Hazard_Detection.v
+++ src/design/Hazard_Detection.v
@@ -25,26 +25,25 @@
 module Hazard_Detection(
 
     input branch_sel, jump_sel, ID_EX_mem_read_en,
-    input [4:0] IF_ID_rs, IF_ID_rt, ID_EX_reg_dest,
+    input [4:0] IF_ID_rs_addr, IF_ID_rt_addr, ID_EX_reg_dest,
 
     output reg flush, stall
     );
     
     always @(*) begin
         
-		/** Load-Use Read after Write (RAW) Data Hazard **/
-		
-		if ( (ID_EX_mem_read_en == 1'b1) &
-			( (ID_EX_reg_dest == IF_ID_rs) | (ID_EX_reg_dest == IF_ID_rt) ) )
-			stall = 1'b1;
-		
-		else stall = 1'b0;
+        /** Load-Use Read after Write (RAW) Data Hazard **/
+        
+        if ( (ID_EX_mem_read_en == 1'b1) &
+            ( (ID_EX_reg_dest == IF_ID_rs_addr) | (ID_EX_reg_dest == IF_ID_rt_addr) ) )
+            stall = 1'b1;
+        
+        else stall = 1'b0;
 	
 	
-	    /** Control Hazard **/
-		
-		flush = (branch_sel | jump_sel);
+	/** Control Hazard **/
+        
+        flush = (branch_sel | jump_sel);
 	
     end
 endmodule
-
