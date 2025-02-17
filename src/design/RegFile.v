55c5eddf889a59eb25590fd5abc9fea0764e9aad
KirbyBurkeJr <108311405+KirbyBurkeJr@users.noreply.github.com>
2025-02-16 17:08:39 -0800

    Update RegFile.v
    Updated register input port names.

--- src/design/RegFile.v
+++ src/design/RegFile.v
@@ -23,7 +23,7 @@
 module RegFile(
     input clk, reset,
 	input reg_write_en,
-    input [4:0] reg_read_addr_1, reg_read_addr_2, reg_write_addr,
+    input [4:0] reg_rs_addr, reg_rt_addr, reg_write_addr,
     input [31:0] reg_write_data,
 
     output [31:0] reg1, reg2 
@@ -33,46 +33,46 @@ module RegFile(
   
     always @ (negedge clk) begin  
 	    if (reset) begin  
-			reg_array[0]  <= 32'b0;  
-			reg_array[1]  <= 32'b0;  
-			reg_array[2]  <= 32'b0;  
-			reg_array[3]  <= 32'b0;  
-			reg_array[4]  <= 32'b0;  
-			reg_array[5]  <= 32'b0;  
-			reg_array[6]  <= 32'b0;  
-			reg_array[7]  <= 32'b0; 
-			reg_array[8]  <= 32'b0;  
-			reg_array[9]  <= 32'b0;  
-			reg_array[10] <= 32'b0;  
-			reg_array[11] <= 32'b0;  
-			reg_array[12] <= 32'b0;  
-			reg_array[13] <= 32'b0;  
-			reg_array[14] <= 32'b0;  
-			reg_array[15] <= 32'b0;
-			reg_array[16] <= 32'b0;  
-			reg_array[17] <= 32'b0; 
-			reg_array[18] <= 32'b0;  
-			reg_array[19] <= 32'b0;  
-			reg_array[20] <= 32'b0; 
-			reg_array[21] <= 32'b0;  
-			reg_array[22] <= 32'b0;  
-			reg_array[23] <= 32'b0;  
-			reg_array[24] <= 32'b0;  
-			reg_array[25] <= 32'b0;  
-			reg_array[26] <= 32'b0;  
-			reg_array[27] <= 32'b0; 
-			reg_array[28] <= 32'b0;  
-			reg_array[29] <= 32'b0;  
-			reg_array[30] <= 32'b0;  
-			reg_array[31] <= 32'b0;     
+            reg_array[0]  <= 32'b0;  
+            reg_array[1]  <= 32'b0;  
+            reg_array[2]  <= 32'b0;  
+            reg_array[3]  <= 32'b0;  
+            reg_array[4]  <= 32'b0;  
+            reg_array[5]  <= 32'b0;  
+            reg_array[6]  <= 32'b0;  
+            reg_array[7]  <= 32'b0; 
+            reg_array[8]  <= 32'b0;  
+            reg_array[9]  <= 32'b0;  
+            reg_array[10] <= 32'b0;  
+            reg_array[11] <= 32'b0;  
+            reg_array[12] <= 32'b0;  
+            reg_array[13] <= 32'b0;  
+            reg_array[14] <= 32'b0;  
+            reg_array[15] <= 32'b0;
+            reg_array[16] <= 32'b0;  
+            reg_array[17] <= 32'b0; 
+            reg_array[18] <= 32'b0;  
+            reg_array[19] <= 32'b0;  
+            reg_array[20] <= 32'b0; 
+            reg_array[21] <= 32'b0;  
+            reg_array[22] <= 32'b0;  
+            reg_array[23] <= 32'b0;  
+            reg_array[24] <= 32'b0;  
+            reg_array[25] <= 32'b0;  
+            reg_array[26] <= 32'b0;  
+            reg_array[27] <= 32'b0; 
+            reg_array[28] <= 32'b0;  
+            reg_array[29] <= 32'b0;  
+            reg_array[30] <= 32'b0;  
+            reg_array[31] <= 32'b0;     
         end  
         
-		else begin  
-			if (reg_write_en) reg_array[reg_write_addr] <= reg_write_data;  
-	    end  
+	else begin  
+            if (reg_write_en) reg_array[reg_write_addr] <= reg_write_data;  
+	end  
     end
 	
-    assign reg1 = (reg_read_addr_1 == 0)? 32'b0 : reg_array[reg_read_addr_1];  
-    assign reg2 = (reg_read_addr_2 == 0)? 32'b0 : reg_array[reg_read_addr_2];  
+    assign reg1 = (reg_rs_addr == 0)? 32'b0 : reg_array[reg_rs_addr];  
+    assign reg2 = (reg_rt_addr == 0)? 32'b0 : reg_array[reg_rt_addr];  
 
 endmodule
